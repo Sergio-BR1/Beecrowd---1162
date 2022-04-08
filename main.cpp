489f530905e0eeb97a3203d4868e57b184953559
Sérgio Brito <71470010+Sergio-BR1@users.noreply.github.com>
2022-04-08 01:39:45 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+
+	cin>>N;
+
+	for (int i=0; i<N; i++) {
+		int L;
+		cin>>L;
+		int vagoes[L];
+		for(int j=0; j<L; j++) {
+			int vag;
+			cin>>vag;
+			vagoes[j] = vag;
+		}
+		int count = 0;
+		for (int k=0; k<L; k++) {
+			int aux;
+			for (int m=k+1; m<L; m++){
+				if (vagoes[k] > vagoes[m]) {
+					aux = vagoes[k];
+					vagoes[k] = vagoes[m];
+					vagoes[m] = aux;
+					count++;
+				}
+			}
+		}
+		cout << "Optimal train swapping takes " << count << " swaps." << endl;
+	}
+		
+}
